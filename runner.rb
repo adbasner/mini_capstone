07a7ade7b4bffc0f1237f6cb6f09b67604b3c19d
Andrew Basner <andrewbasner@gmail.com>
2018-07-01 09:43:22 -0400

    clean up runner

--- runner.rb
+++ runner.rb
@@ -6,53 +6,36 @@ require 'faker'
 # puts JSON.pretty_generate(response.body)
 
 # create
-p '*' * 80
 
-p 'Do you want to create a product, yes or no?'
-answer = gets.chomp.downcase
-if answer == 'yes'
-  response = Unirest.post('http://localhost:3000/api/products',
-    parameters: {
-      input_name: Faker::Coffee.blend_name,
-      input_description: Faker::Coffee.notes,
-      input_price: rand(1..99),
-      input_image_url: Faker::Internet.url
-    })
+response = Unirest.post('http://localhost:3000/api/products',
+  parameters: {
+    input_name: Faker::Coffee.blend_name,
+    input_description: Faker::Coffee.notes,
+    input_price: rand(1..99),
+    input_image_url: Faker::Internet.url
+  })
 
-  p response.body
-end
+p response.body
 
 # update
-p '*' * 80
 
-p 'Do you want to update a product, yes or no?'
-answer2 = gets.chomp.downcase
-if answer2 == 'yes'
-  p 'What coffee id do you want to change?'
-  product_id = gets.chomp
-  p 'Enter a new name then new price.'
-  response = Unirest.patch("localhost:3000/api/products/#{product_id}",
-    parameters: {
-      input_name: gets.chomp,
-      input_price: gets.chomp.to_i
-    })
+p 'What coffee id do you want to change?'
+product_id = gets.chomp
+p 'Enter a new name then new price.'
+response = Unirest.patch("localhost:3000/api/products/#{product_id}",
+  parameters: {
+    input_name: gets.chomp,
+    input_price: gets.chomp.to_i
+  })
 
-  p response.body
-end
+p response.body
 
 # destroy
-p '*' * 80
 
-p 'Do you want to delete a product, yes or no?'
-answer3 = gets.chomp.downcase
-if answer3 == 'yes'
-  p 'What coffee id do you want to delete?'
-  product_id = gets.chomp
+p 'What coffee id do you want to delete?'
+product_id = gets.chomp
 
-  response = Unirest.delete("localhost:3000/api/products/#{product_id}")
+response = Unirest.delete("localhost:3000/api/products/#{product_id}")
 
-  p "You deleted coffee ##{product_id}."
-  p response.body
-end
-
-p '*' * 80
+p "You deleted coffee ##{product_id}."
+p response.body
