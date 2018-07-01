abe85f074e9b18b37ff350077f0e68f41569188a
Andrew Basner <andrewbasner@gmail.com>
2018-07-01 10:34:49 -0400

    clear up runner, add option to create or delete, asking yes or no

--- runner.rb
+++ runner.rb
@@ -7,45 +7,80 @@ require 'faker'
 
 # create
 
-p 'Enter a coffee name:'
-name = gets.chomp
+p '-' * 50
+p 'Do you want to enter a new coffee? Type yes if you do.'
+create = gets.chomp.downcase
 
-p 'Enter a coffee description:'
-description = gets.chomp
+if create == 'yes'
+  p '-' * 50
+  p 'Enter a coffee name:'
+  name = gets.chomp
+  p '-' * 50
 
-p 'Enter a price (integer):'
-price = gets.chomp.to_i
+  p 'Enter a coffee description:'
+  description = gets.chomp
+  p '-' * 50
 
-p 'Etner an image url:'
-image_url = gets.chomp
+  p 'Enter a price (integer):'
+  price = gets.chomp.to_i
+  p '-' * 50
 
-response = Unirest.post('http://localhost:3000/api/products',
-  parameters: {
-    # input_name: Faker::Coffee.blend_name,
-    # input_description: Faker::Coffee.notes,
-    # input_price: rand(1..99),
-    # input_image_url: Faker::Internet.url
-    input_name: name,
-    input_description: description,
-    input_price: price,
-    input_image_url: image_url
-  })
+  p 'Enter an image url:'
+  image_url = gets.chomp
+  p '-' * 50
 
-p response.body
+  response = Unirest.post('http://localhost:3000/api/products',
+    parameters: {
+      # input_name: Faker::Coffee.blend_name,
+      # input_description: Faker::Coffee.notes,
+      # input_price: rand(1..99),
+      # input_image_url: Faker::Internet.url
+      input_name: name,
+      input_description: description,
+      input_price: price,
+      input_image_url: image_url
+    })
+
+  p 'New product created.'
+  p response.body
+  p '-' * 50
+end
 
 # update
+p 'Do you want to update new coffee? Type yes if you do.'
+update = gets.chomp.downcase
+p '-' * 50
 
-# p 'What coffee id do you want to change?'
-# product_id = gets.chomp
-# p 'Enter a new name then new price.'
-# response = Unirest.patch("localhost:3000/api/products/#{product_id}",
-#   parameters: {
-#     input_name: gets.chomp,
-#     input_price: gets.chomp.to_i
-#   })
+if update == 'yes'
 
-# p response.body
+  p 'What coffee id do you want to change?'
+  product_id = gets.chomp
+  p '-' * 50
 
+  p 'Enter a new name:'
+  update_name = gets.chomp
+  p '-' * 50
+
+  p 'Enter a new description'
+  update_description = gets.chomp
+  p '-' * 50
+
+  p 'Enter a new price'
+  update_price = gets.chomp.to_i
+  p '-' * 50
+
+  response = Unirest.patch("localhost:3000/api/products/#{product_id}",
+    parameters: {
+      input_name: update_name,
+      input_description: update_description,
+      input_price: update_price
+    })
+
+  p 'Product has been updated'
+  p response.body
+  p '-' * 50
+
+end
 # # destroy
 
 # p 'What coffee id do you want to delete?'
